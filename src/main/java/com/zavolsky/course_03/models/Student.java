9268c6db9e3d120400503437655721c8e2da8699
vzavolsky <vzavolsky@ya.ru>
2023-05-29 18:11:46 +0300

    Course 03 - HW 003 - Database.

--- src/main/java/com/zavolsky/course_03/models/Student.java
+++ src/main/java/com/zavolsky/course_03/models/Student.java
@@ -13,15 +13,6 @@ public class Student {
     private String name;
     private int age;
 
-    public Student(Long id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public Student() {
-    }
-
     public Long getId() {
         return id;
     }
